582ad1e3f15b243b4bd98e5ad9cfabb535e6f5b9
abhinav2188 <abhinav.mca19.du@gmail.com>
2020-02-25 18:36:26 +0530

    add delete feature

--- src/components/note.jsx
+++ src/components/note.jsx
@@ -5,6 +5,8 @@ export default function Note(data){
         <div className="bg-white m-4 shadow-lg rounded max-w-sm">
             <h1 className="text-xl mt-1 mx-2 p-1">{data.title}</h1>
             <p className="mt-1 mx-2 mb-2 p-1">{data.content}</p>
+            <button className="p-1 mr-2 mb-1 text-xs float-right" type="button" onClick={()=> data.onDelete(data.id)}>DELETE</button>
+
         </div>
     );
 
--- src/components/notesContainer.jsx
+++ src/components/notesContainer.jsx
@@ -1,7 +1,6 @@
 import React,{useState} from 'react';
 import NoteItem from './note.jsx';
 import AddNote from './addNote.jsx';
-import notesdata from '../notes.js';
 
 
 export default function NotesContainer(props){
@@ -9,13 +8,17 @@ export default function NotesContainer(props){
     function handleAddNote(newNote){
         setNotesList(prevList => ([...prevList,newNote]) );
     }
+    function handleOnDelete(id){
+        console.log('delete',id);
+        setNotesList(prevList => ( prevList.filter( (note,index) => (index !== id))) );
+    }
 
     return(
         <div>
             <AddNote onAdd={handleAddNote}/>
-            <div className="flex p-8 flex-wrap items-start">
-                {notesList.map((note,index) => (<NoteItem key={index} title={note.title} content={note.content} />) )}
-                {notesdata.map(note => (<NoteItem key={note.id} title={note.title} content={note.content} />) )}
+            <div className="flex p-6 flex-wrap items-start">
+            {/* <NoteItem title="" content="Proin mattis sagittis euismod. Praesent vel mi nisi. Phasellus volutpat viverra velit, sed consectetur augue euismod sit amet. In a lectus id est gravida convallis sit amet quis lorem. Suspendisse tristique nunc id fringilla pulvinar. Maecenas rhoncus sollicitudin condimentum." onDelete={handleOnDelete}/> */}
+                {notesList.map((note,index) => (<NoteItem key={index} id={index} title={note.title} content={note.content} onDelete={handleOnDelete}/>) )}
             </div>
         </div>
     );

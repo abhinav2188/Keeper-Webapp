4c1e11d9c27065673ca9d3ceccf54bbf27571896
abhinav2188 <abhinav.mca19.du@gmail.com>
2020-04-18 17:40:13 +0530

    deploy commit

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-
-app.use(bodyParser.urlencoded({extended:true}));
-
-app.listen(3001,() => console.log("server started at port 3001"));

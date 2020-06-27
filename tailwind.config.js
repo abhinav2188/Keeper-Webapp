f306ee081a52c576e3fe9f389d7c74899771739b
abhinav2188 <abhinav.mca19.du@gmail.com>
2020-06-27 13:46:36 +0530

    deploy changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,26 @@
 module.exports = {
+  purge: [],
   theme: {
-    inset :{
-      '0' : '0',
-      '1/5' : '20%',
-      '1/2' : '50%',
-      '1/10' : '10%'
+  inset :{
+    '0' : '0',
+    '1/5' : '20%',
+    '1/2' : '50%',
+    '1/10' : '10%'
+  },
+  fontSize: {
+    '3xs': '.5rem',
+    '2xs': '.625rem',
+    'xs': '.75rem',
+    'sm': '.875rem',
+    'base': '1rem',
+    'lg': '1.125rem',
+    'xl': '1.25rem',
+    '2xl': '1.5rem',
+    '3xl': '1.875rem',
+    '4xl': '2.25rem',
+    '5xl': '3rem',
+    '6xl': '4rem',
     },
-    fontSize: {
-      '3xs': '.5rem',
-      '2xs': '.625rem',
-      'xs': '.75rem',
-      'sm': '.875rem',
-      'base': '1rem',
-      'lg': '1.125rem',
-      'xl': '1.25rem',
-      '2xl': '1.5rem',
-      '3xl': '1.875rem',
-      '4xl': '2.25rem',
-      '5xl': '3rem',
-      '6xl': '4rem',
-      },
-
     extend: {},
   },
   variants: {},
